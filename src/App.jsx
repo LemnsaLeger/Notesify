a05eac4af6936b1bbcad31faa3b8d5e5da18dd71
LemnsaLeger <lemnsadigitalservices@gmail.com>
2026-03-31 11:52:39 +0100

    Modified: src/App.jsx, desc: Removed redundant notes display and added the NoteEditor component to the main App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 // src/App.jsx
-import { useNotes } from './context/NotesContext';
-import  NoteCard  from "./components/notes/NoteCard";
 import NoteList from "./components/notes/NoteList";
+import NoteEditor from './components/notes/NoteEditor';
 
 function App() {
-  const { notes} = useNotes();
 
   return (
     <div className="flex h-screen bg-[#0F0F1A] text-white overflow-hidden">
       <NoteList />
-      <main className="min-h-screen bg-[#0F0F1A] text-white p-8">
-        <section className="flex flex-col gap-3 w-72">
-          {notes?.map((note) => (
-            <NoteCard key={note.id} note={note} />
-          ))}
-        </section>
-      </main>
+      <NoteEditor />
     </div>
   );
 }
