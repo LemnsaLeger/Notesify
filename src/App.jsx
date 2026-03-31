7f2a41a5ce01aece738f723c6fffd82a2e6d8de6
LemnsaLeger <lemnsadigitalservices@gmail.com>
2026-03-31 10:14:47 +0100

    Modified: src/App.jsx, desc: Displayed the notes in the UI. Showing the selected notes by default and allowing the user to select other notes to display.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 // src/App.jsx
-import { useNotes } from './context/NotesContext'
+import { useNotes } from './context/NotesContext';
+import  NoteCard  from "./components/notes/NoteCard";
 
 function App() {
-  const { notes, selectedNote} = useNotes();
+  const { notes} = useNotes();
 
   return (
     <main className="min-h-screen bg-[#0F0F1A] text-white p-8">
       <h1 className="text-2xl font-bold">Notesify</h1>
-     <p>{notes.length} notes</p>
-     <p>{selectedNote?.title ?? 'No note selected'}</p>
+
+      <section className="flex flex-col gap-3 w-72">
+        {notes?.map((note) => (
+          <NoteCard 
+          key={note.id}
+          note={note}
+          />
+        ))}
+      </section>
+
     </main>
   )
 }
