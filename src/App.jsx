dc08d3f9c77c7ae840c4711d8397eb19003800d5
LemnsaLeger <lemnsadigitalservices@gmail.com>
2026-03-31 11:28:55 +0100

    Modified: src/App.jsx, desc: Display a sidebar with a list of notes,a search bar and filters tags. Make them work!

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 // src/App.jsx
 import { useNotes } from './context/NotesContext';
 import  NoteCard  from "./components/notes/NoteCard";
+import NoteList from "./components/notes/NoteList";
 
 function App() {
   const { notes} = useNotes();
 
   return (
-    <main className="min-h-screen bg-[#0F0F1A] text-white p-8">
-      <h1 className="text-2xl font-bold">Notesify</h1>
-
-      <section className="flex flex-col gap-3 w-72">
-        {notes?.map((note) => (
-          <NoteCard 
-          key={note.id}
-          note={note}
-          />
-        ))}
-      </section>
-
-    </main>
-  )
+    <div className="flex h-screen bg-[#0F0F1A] text-white overflow-hidden">
+      <NoteList />
+      <main className="min-h-screen bg-[#0F0F1A] text-white p-8">
+        <section className="flex flex-col gap-3 w-72">
+          {notes?.map((note) => (
+            <NoteCard key={note.id} note={note} />
+          ))}
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App
