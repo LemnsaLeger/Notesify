dbb53cadacc5bf1dc2882d9524ae3c188dd07a88
LemnsaLeger <lemnsadigitalservices@gmail.com>
2026-04-27 16:45:48 +0100

    Modified: src/main.jsx, desc: Wrapped the App with AuthProvider to provide authentication context to the entire application.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { NotesProvider } from './context/NotesContext.jsx'
+import { AuthProvider } from './context/authcontex.jsx';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <NotesProvider>
-      <App />
-    </NotesProvider>
+    <AuthProvider>
+      <NotesProvider>
+        <App />
+      </NotesProvider>
+    </AuthProvider>
   </StrictMode>,
 );
