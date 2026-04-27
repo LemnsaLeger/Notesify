6bd52e69c557c8589702fc2ed4ee0c7a7c9118fc
LemnsaLeger <lemnsadigitalservices@gmail.com>
2026-04-27 16:52:22 +0100

    Modified:  src/App.jsx, desc: Add logic to show auth if not logged in, and show the app if logged in.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 // src/App.jsx
+import { useAuth } from "./context/authcontex.jsx";
+import AuthPage from "./components/auth/authpage.jsx";
 import NoteList from "./components/notes/NoteList";
-import NoteEditor from './components/notes/NoteEditor';
+import NoteEditor from "./components/notes/NoteEditor";
 import AIPanel from "./components/ai/AIPanel";
 
 function App() {
+  const { user, loading, signOut } = useAuth();
 
+  // Show nothing while Supabase checks session
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-[#0F0F1A] flex items-center justify-center">
+        <p className="text-white/20 text-sm animate-pulse">Loading...</p>
+      </div>
+    );
+  }
+
+  // Not logged inshow auth page
+  if (!user) return <AuthPage />;
+
+  // Logged inshow the app
   return (
     <div className="flex h-screen bg-[#0F0F1A] text-white overflow-hidden">
       <NoteList />
@@ -14,4 +30,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
